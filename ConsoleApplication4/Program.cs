b73f81ee9aed5ad3a4fa8036616d51deaa35efa2
lamnaourm <mohammed.lamnaour@gmail.com>
2022-06-11 10:31:34 +0100

    call API Dotnet

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest client = HttpWebRequest.Create("http://localhost:8080/Ex2WebService/rest/employes/allemps");
+            client.ContentType = "application/json";
+            client.Method = "GET";
+
+            WebResponse s = client.GetResponse();
+            var r = new StreamReader(s.GetResponseStream());
+
+            String data = r.ReadToEnd();
+            Console.WriteLine(data);
+
+            r.Close();
+            s.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
